3b4a65105bfc2eef4056ad3505c7fb1155d7571e
akasbyju0 <94997492+akasbyju0@users.noreply.github.com>
2022-04-26 18:45:49 -0400

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,18 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCzLN0HzuNeOw_ad1Kk8jRNM6a_UBuO7kA",
+    authDomain: "classtest-a5abb.firebaseapp.com",
+    databaseURL: "https://classtest-a5abb-default-rtdb.firebaseio.com",
+    projectId: "classtest-a5abb",
+    storageBucket: "classtest-a5abb.appspot.com",
+    messagingSenderId: "54590184475",
+    appId: "1:54590184475:web:38cf67dd4b04be028becbe"
+}; // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+function addUser() {
+    user_name = document.getElementById("user_name").value;
+    firebase.database().ref("/").child(user_name).update({
+        purpose: "adding user"
+    });
+}
